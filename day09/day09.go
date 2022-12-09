88f3c2c8456d297e23eba583af103bcb90c60897
Maxence <max@23.tf>
2022-12-09 08:00:11 -0500

    day09 p1

---
+++ day09/day09.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	// "github.com/nbr23/advent-of-code-2022/utils/inputs"
+
+	"strings"
+
+	"github.com/nbr23/advent-of-code-2022/utils/inputs"
+	"github.com/nbr23/advent-of-code-2022/utils/utils"
+	//	"github.com/pkg/profile"
+)
+
+var day_num int = 9
+
+type Point struct {
+	x, y int
+}
+
+func getVector(dir string) Point {
+	switch dir {
+	case "U":
+		return Point{0, 1}
+	case "D":
+		return Point{0, -1}
+	case "L":
+		return Point{-1, 0}
+	case "R":
+		return Point{1, 0}
+	}
+	panic("Unsupported move")
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return -1 * i
+	}
+	return i
+}
+
+func part1(input string) interface{} {
+	var T Point
+	var H Point
+	visited := make(map[Point]bool)
+	visited[T] = true
+
+	for _, line := range inputs.InputToStrList(input) {
+		instr := strings.Split(line, " ")
+		moves := inputs.ParseDecInt(instr[1])
+		for i := 0; i < moves; i++ {
+			v := getVector(instr[0])
+			H.x += v.x
+			H.y += v.y
+			// Move tail
+			diff := Point{abs(T.x - H.x), abs(T.y - H.y)}
+			if diff.x <= 1 && diff.y <= 1 {
+				// no action
+				continue
+			}
+			// aligned, so just increment as needed
+			if diff.x == 0 && H.y > T.y {
+				T.y++
+			} else if diff.x == 0 && H.y < T.y {
+				T.y--
+			} else if diff.y == 0 && H.x > T.x {
+				T.x++
+			} else if diff.y == 0 && H.x < T.x {
+				T.x--
+			} else {
+				// Diagonals
+				if H.x > T.x {
+					T.x++
+				} else {
+					T.x--
+				}
+				if H.y > T.y {
+					T.y++
+				} else {
+					T.y--
+				}
+			}
+			visited[T] = true
+		}
+	}
+	return len(visited)
+}
+
+func part2(input string) interface{} {
+	return nil
+}
+
+func main() {
+	// defer profile.Start().Stop()
+	utils.Solve(part1, part2, day_num)
+}
