6a871093623e3e52149f7c8aff779af450e81471
Maxence <max@23.tf>
2022-12-09 08:08:28 -0500

    p2

--- day09/day09.go
+++ day09/day09.go
@@ -85,7 +85,63 @@ func part1(input string) interface{} {
 }
 
 func part2(input string) interface{} {
-	return nil
+	Tails := make([]Point, 9)
+	var Head Point
+	visited := make(map[Point]bool)
+	visited[Tails[8]] = true
+
+	for _, line := range inputs.InputToStrList(input) {
+		instr := strings.Split(line, " ")
+		moves := inputs.ParseDecInt(instr[1])
+		for i := 0; i < moves; i++ {
+			v := getVector(instr[0])
+			Head.x += v.x
+			Head.y += v.y
+			// Move tail
+			for ti := range Tails {
+				var H *Point
+				var T *Point
+				if ti == 0 {
+					H = &Head
+				} else {
+					H = &Tails[ti-1]
+				}
+				T = &Tails[ti]
+
+				diff := Point{abs(T.x - H.x), abs(T.y - H.y)}
+				if diff.x <= 1 && diff.y <= 1 {
+					// no action
+					continue
+				}
+				// aligned, so just increment as needed
+				if diff.x == 0 && H.y > T.y {
+					T.y++
+				} else if diff.x == 0 && H.y < T.y {
+					T.y--
+				} else if diff.y == 0 && H.x > T.x {
+					T.x++
+				} else if diff.y == 0 && H.x < T.x {
+					T.x--
+				} else {
+					// Diagonals
+					if H.x > T.x {
+						T.x++
+					} else {
+						T.x--
+					}
+					if H.y > T.y {
+						T.y++
+					} else {
+						T.y--
+					}
+				}
+				if ti == 8 {
+					visited[Tails[ti]] = true
+				}
+			}
+		}
+	}
+	return len(visited)
 }
 
 func main() {
