5ac6e0fccba1cb1c8b20892e3ee06a21929250c9
Maxence <max@23.tf>
2022-12-18 16:06:41 -0500

    day18 p2

--- day18/day18.go
+++ day18/day18.go
@@ -24,8 +24,24 @@ var NEIGHBORS = []Point{
 	{0, 0, -1},
 }
 
-func parseCubes(input string) int {
-	res := 0
+func countNeightbors(cubesMap map[Point]bool, c Point) int {
+	count := 0
+	for _, neighbor := range NEIGHBORS {
+		if _, ok := cubesMap[Point{c.x + neighbor.x, c.y + neighbor.y, c.z + neighbor.z}]; ok {
+			count++
+		}
+	}
+	return count
+}
+
+type Bound struct {
+	min, max int
+}
+
+func parseCubes(input string) (map[Point]bool, Bound, Bound, Bound) {
+	boundX := Bound{100, 0} // min max
+	boundY := Bound{100, 0}
+	boundZ := Bound{100, 0}
 	cubes := make(map[Point]bool)
 	for _, line := range inputs.InputToStrList(input) {
 		coords := strings.Split(line, ",")
@@ -34,12 +50,22 @@ func parseCubes(input string) int {
 			inputs.ParseDecInt(coords[1]),
 			inputs.ParseDecInt(coords[2]),
 		}
+		boundX = Bound{utils.IntMin(boundX.min, cube.x), utils.IntMax(boundX.max, cube.x)}
+		boundY = Bound{utils.IntMin(boundY.min, cube.y), utils.IntMax(boundY.max, cube.y)}
+		boundZ = Bound{utils.IntMin(boundZ.min, cube.z), utils.IntMax(boundZ.max, cube.z)}
 
 		cubes[cube] = true
+	}
+	return cubes, Bound{boundX.min - 1, boundX.max + 1}, Bound{boundY.min - 1, boundY.max + 1}, Bound{boundZ.min - 1, boundZ.max + 1}
+}
+
+func countExposed(cubes map[Point]bool) int {
+	res := 0
+	for cube := range cubes {
 		res += 6
 		for _, neighbor := range NEIGHBORS {
 			if _, ok := cubes[Point{cube.x + neighbor.x, cube.y + neighbor.y, cube.z + neighbor.z}]; ok {
-				res -= 2
+				res -= 1
 			}
 		}
 	}
@@ -47,11 +73,40 @@ func parseCubes(input string) int {
 }
 
 func part1(input string) interface{} {
-	return parseCubes(input)
+	cubes, _, _, _ := parseCubes(input)
+	return countExposed(cubes)
+}
+
+func addPoints(p1, p2 Point) Point {
+	return Point{p1.x + p2.x, p1.y + p2.y, p1.z + p2.z}
+}
+
+func steamSpread(cubes map[Point]bool, visited map[Point]bool, current Point, boundX, boundY, boundZ Bound, res *int) {
+	for _, nb := range NEIGHBORS {
+		neighbor := addPoints(current, nb)
+		if neighbor.x > boundX.max || neighbor.x < boundX.min ||
+			neighbor.y > boundY.max || neighbor.y < boundY.min ||
+			neighbor.z > boundZ.max || neighbor.z < boundZ.min {
+			continue
+		}
+		if _, ok := visited[neighbor]; ok {
+			continue
+		}
+		if _, ok := cubes[neighbor]; ok {
+			*res += 1
+			continue
+		}
+		visited[neighbor] = true
+		steamSpread(cubes, visited, neighbor, boundX, boundY, boundZ, res)
+	}
 }
 
 func part2(input string) interface{} {
-	return nil
+	res := 0
+	cubes, boundX, boundY, boundZ := parseCubes(input)
+	visited := make(map[Point]bool)
+	steamSpread(cubes, visited, Point{boundX.min, boundY.min, boundZ.min}, boundX, boundY, boundZ, &res)
+	return res
 }
 
 func main() {
