abfb9782f8439034fd5f5a4fb35220638d5f42fe
Maxence <max@23.tf>
2022-12-21 22:10:47 -0500

    day17p2

--- day17/day17.go
+++ day17/day17.go
@@ -16,94 +16,134 @@ type Point struct {
 }
 
 type Rock struct {
-	width   int
-	height  int
-	rockfun RockFunc
+	width     int
+	height    int
+	rockfun   RockFunc
+	rockfun_i RockFun_i
+	points    []*Point
+	points_i  []int
 }
 
 // -
-func Rock_1(coord Point) []Point {
-	return []Point{
-		Point{coord.X, coord.Y},
-		Point{coord.X + 1, coord.Y},
-		Point{coord.X + 2, coord.Y},
-		Point{coord.X + 3, coord.Y},
-	}
+func Rock_1(coord Point, pts *[]*Point) {
+	(*pts)[0].X, (*pts)[0].Y = coord.X, coord.Y
+	(*pts)[1].X, (*pts)[1].Y = coord.X+1, coord.Y
+	(*pts)[2].X, (*pts)[2].Y = coord.X+2, coord.Y
+	(*pts)[3].X, (*pts)[3].Y = coord.X+3, coord.Y
+}
+func Rock_1_i(coord Point, pts *[]int) {
+	(*pts)[0] = coord.Y*7 + coord.X
+	(*pts)[1] = coord.Y*7 + coord.X + 1
+	(*pts)[2] = coord.Y*7 + coord.X + 2
+	(*pts)[3] = coord.Y*7 + coord.X + 3
 }
 
 var ROCK_1 = Rock{
 	4,
 	1,
 	Rock_1,
+	Rock_1_i,
+	make([]*Point, 4),
+	make([]int, 4),
 }
 
 // +
-func Rock_2(coord Point) []Point {
-	return []Point{
-		Point{coord.X + 1, coord.Y},
-		Point{coord.X, coord.Y + 1},
-		Point{coord.X + 1, coord.Y + 1},
-		Point{coord.X + 2, coord.Y + 1},
-		Point{coord.X + 1, coord.Y + 2},
-	}
+func Rock_2(coord Point, pts *[]*Point) {
+	(*pts)[0].X, (*pts)[0].Y = coord.X+1, coord.Y
+	(*pts)[1].X, (*pts)[1].Y = coord.X, coord.Y+1
+	(*pts)[2].X, (*pts)[2].Y = coord.X+2, coord.Y+1
+	(*pts)[3].X, (*pts)[3].Y = coord.X+1, coord.Y+2
+}
+
+func Rock_2_i(coord Point, pts *[]int) {
+	(*pts)[0] = coord.Y*7 + coord.X + 1
+	(*pts)[1] = (coord.Y+1)*7 + coord.X
+	(*pts)[2] = (coord.Y+1)*7 + coord.X + 2
+	(*pts)[3] = (coord.Y+2)*7 + coord.X + 1
 }
 
 var ROCK_2 = Rock{
 	3,
 	1,
 	Rock_2,
+	Rock_2_i,
+	make([]*Point, 4),
+	make([]int, 4),
 }
 
 // L backwards
-func Rock_3(coord Point) []Point {
-	return []Point{
-		Point{coord.X + 2, coord.Y + 2},
-		Point{coord.X + 2, coord.Y + 1},
-		Point{coord.X, coord.Y},
-		Point{coord.X + 1, coord.Y},
-		Point{coord.X + 2, coord.Y},
-	}
+func Rock_3(coord Point, pts *[]*Point) {
+	(*pts)[0].X, (*pts)[0].Y = coord.X+2, coord.Y+2
+	(*pts)[1].X, (*pts)[1].Y = coord.X+2, coord.Y+1
+	(*pts)[2].X, (*pts)[2].Y = coord.X, coord.Y
+	(*pts)[3].X, (*pts)[3].Y = coord.X+1, coord.Y
+	(*pts)[4].X, (*pts)[4].Y = coord.X+2, coord.Y
+}
+func Rock_3_i(coord Point, pts *[]int) {
+	(*pts)[0] = (coord.Y+2)*7 + coord.X + 2
+	(*pts)[1] = (coord.Y+1)*7 + coord.X + 2
+	(*pts)[2] = (coord.Y)*7 + coord.X
+	(*pts)[3] = (coord.Y)*7 + coord.X + 1
+	(*pts)[4] = (coord.Y)*7 + coord.X + 2
 }
 
 var ROCK_3 = Rock{
 	3,
 	1,
 	Rock_3,
+	Rock_3_i,
+	make([]*Point, 5),
+	make([]int, 5),
 }
 
 // l
-func Rock_4(coord Point) []Point {
-	return []Point{
-		Point{coord.X, coord.Y},
-		Point{coord.X, coord.Y + 1},
-		Point{coord.X, coord.Y + 2},
-		Point{coord.X, coord.Y + 3},
-	}
+func Rock_4(coord Point, pts *[]*Point) {
+	(*pts)[0].X, (*pts)[0].Y = coord.X, coord.Y
+	(*pts)[1].X, (*pts)[1].Y = coord.X, coord.Y+1
+	(*pts)[2].X, (*pts)[2].Y = coord.X, coord.Y+2
+	(*pts)[3].X, (*pts)[3].Y = coord.X, coord.Y+3
+}
+func Rock_4_i(coord Point, pts *[]int) {
+	(*pts)[0] = (coord.Y)*7 + coord.X
+	(*pts)[1] = (coord.Y+1)*7 + coord.X
+	(*pts)[2] = (coord.Y+2)*7 + coord.X
+	(*pts)[3] = (coord.Y+3)*7 + coord.X
 }
 
 var ROCK_4 = Rock{
 	1,
 	1,
 	Rock_4,
+	Rock_4_i,
+	make([]*Point, 4),
+	make([]int, 4),
 }
 
 // square
-func Rock_5(coord Point) []Point {
-	return []Point{
-		Point{coord.X, coord.Y},
-		Point{coord.X + 1, coord.Y},
-		Point{coord.X, coord.Y + 1},
-		Point{coord.X + 1, coord.Y + 1},
-	}
+func Rock_5(coord Point, pts *[]*Point) {
+	(*pts)[0].X, (*pts)[0].Y = coord.X, coord.Y
+	(*pts)[1].X, (*pts)[1].Y = coord.X+1, coord.Y
+	(*pts)[2].X, (*pts)[2].Y = coord.X, coord.Y+1
+	(*pts)[3].X, (*pts)[3].Y = coord.X+1, coord.Y+1
+}
+func Rock_5_i(coord Point, pts *[]int) {
+	(*pts)[0] = (coord.Y)*7 + coord.X
+	(*pts)[1] = (coord.Y)*7 + coord.X + 1
+	(*pts)[2] = (coord.Y+1)*7 + coord.X
+	(*pts)[3] = (coord.Y+1)*7 + coord.X + 1
 }
 
 var ROCK_5 = Rock{
 	2,
 	1,
 	Rock_5,
+	Rock_5_i,
+	make([]*Point, 4),
+	make([]int, 4),
 }
 
-type RockFunc func(coord Point) []Point
+type RockFunc func(coord Point, pts *[]*Point)
+type RockFun_i func(coord Point, pts *[]int)
 
 var ROCKS = []Rock{
 	ROCK_1,
@@ -113,11 +153,9 @@ var ROCKS = []Rock{
 	ROCK_5,
 }
 
-func canGoTo(playground map[Point]bool, current_rock *Rock, current_rock_coords Point) bool {
-	for _, coords := range current_rock.rockfun(current_rock_coords) {
-		if coords.X < 0 || coords.X >= 7 {
-			return false
-		}
+func canGoTo(playground Playground, current_rock *Rock, current_rock_coords Point) bool {
+	current_rock.rockfun_i(current_rock_coords, &current_rock.points_i)
+	for _, coords := range current_rock.points_i {
 		if _, ok := playground[coords]; ok {
 			return false
 		}
@@ -125,20 +163,21 @@ func canGoTo(playground map[Point]bool, current_rock *Rock, current_rock_coords 
 	return true
 }
 
-func fillFloor(playground map[Point]bool) {
+func fillFloor(playground Playground) {
 	for i := 0; i <= 7; i++ {
-		playground[Point{i, 0}] = true
+		playground[i] = true
 	}
 }
 
-func printMap(playground map[Point]bool, highest int, current Rock, current_pos Point) {
+func printMap(playground Playground, highest int, current Rock, current_pos Point) {
 	for y := highest; y >= 0; y-- {
 		for x := 0; x < 7; x++ {
-			if _, ok := playground[Point{x, y}]; ok {
+			if _, ok := playground[y*7+x]; ok {
 				fmt.Print("#")
 			} else {
 				done := false
-				for _, pos := range current.rockfun(current_pos) {
+				current.rockfun(current_pos, &current.points)
+				for _, pos := range current.points {
 					if pos.X == x && pos.Y == y {
 						fmt.Print("@")
 						done = true
@@ -154,26 +193,77 @@ func printMap(playground map[Point]bool, highest int, current Rock, current_pos 
 	}
 }
 
+// func garbageCollect(playground Playground, highest int) {
+// 	newfloor := highest
+// 	for x := 0; x < 7; x++ {
+// 		for y := newfloor; y > 0; y-- {
+// 			if _, ok := playground[y*7+x]; ok {
+// 				newfloor = utils.IntMin(newfloor, y)
+// 				break
+// 			}
+// 		}
+// 	}
+// 	// fmt.Printf("Newfloor %d\n", newfloor)
+// 	for pt := range playground { // y*7+x
+// 		if pt < newfloor*7 {
+// 			delete(playground, pt)
+// 		}
+// 	}
+// }
+
+type Playground map[int]bool
+
+func findPattern(heights []int, cycle int) (int, int, int) {
+
+	for p_len := cycle; p_len < cycle*10; p_len++ {
+		for p_start := cycle; p_start+2*p_len < len(heights); p_start++ {
+			good := false
+			diff := heights[p_start+p_len] - heights[p_start]
+
+			for i := p_start; i < p_start+p_len; i++ {
+				currdiff := heights[i+p_len] - heights[i]
+				if currdiff != diff {
+					good = false
+					break
+				}
+				good = true
+			}
+			if good {
+				return p_start, p_len, diff
+			}
+		}
+	}
+	return -1, -1, -1
+}
+
 func dropRocks(input string, MAX int) int {
-	playground := make(map[Point]bool)
+	playground := make(Playground)
+	heights := make([]int, 0)
 	fillFloor(playground)
 	rockindex := 0
 	highest := 0
 	var current_rock *Rock
 	current_rock = nil
-	var current_rock_coord Point // top left coordinates
+	var current_rock_coord Point
 
 	rock_count := MAX + 1
-
 	for i := 0; ; i++ {
 		if current_rock == nil {
 			current_rock = &ROCKS[rockindex]
 			current_rock_coord = Point{2, highest + current_rock.height + 3}
 			rockindex = (rockindex + 1) % len(ROCKS)
 			rock_count--
+
+			heights = append(heights, highest)
 			if rock_count == 0 {
-				fmt.Println("Done?")
 				return highest
+			}
+			if len(input)*5*10 == MAX-rock_count {
+				pattern_start, pattern_len, diff := findPattern(heights, len(input)*5)
+				periodsCount := (MAX - pattern_start) / pattern_len
+				periodsRest := (MAX - pattern_start) % pattern_len
+				return heights[pattern_start] + heights[periodsRest+pattern_start] - heights[pattern_start] + diff*periodsCount
+
 			}
 		}
 
@@ -181,12 +271,12 @@ func dropRocks(input string, MAX int) int {
 
 		// Lateral move
 		if c == '>' {
-			if canGoTo(playground, current_rock, Point{current_rock_coord.X + 1, current_rock_coord.Y}) {
+			if current_rock_coord.X+1+current_rock.width-1 < 7 && canGoTo(playground, current_rock, Point{current_rock_coord.X + 1, current_rock_coord.Y}) {
 				current_rock_coord.X++
 			} else {
 			}
 		} else {
-			if canGoTo(playground, current_rock, Point{current_rock_coord.X - 1, current_rock_coord.Y}) {
+			if current_rock_coord.X-1 >= 0 && canGoTo(playground, current_rock, Point{current_rock_coord.X - 1, current_rock_coord.Y}) {
 				current_rock_coord.X--
 			} else {
 			}
@@ -196,8 +286,9 @@ func dropRocks(input string, MAX int) int {
 		if canGoTo(playground, current_rock, Point{current_rock_coord.X, current_rock_coord.Y - 1}) {
 			current_rock_coord.Y--
 		} else {
-			for _, rock := range current_rock.rockfun(current_rock_coord) {
-				highest = utils.IntMax(highest, rock.Y)
+			current_rock.rockfun_i(current_rock_coord, &current_rock.points_i)
+			for _, rock := range current_rock.points_i {
+				highest = utils.IntMax(highest, rock/7)
 				playground[rock] = true
 			}
 			current_rock = nil
@@ -207,7 +298,6 @@ func dropRocks(input string, MAX int) int {
 }
 
 func part1(input string) interface{} {
-
 	return dropRocks(strings.Trim(input, "\n"), 2022)
 }
 
